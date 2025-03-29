c639054eb2fa14d5c7b765c506349ab6a663b6ea
ganeshburri <ganeshburri1@gmail.com>
2025-03-29 16:08:41 +0530

    feat: add bank-webhook server

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,65 @@
+import express from "express";
+import db from "@repo/db/client";
+const app = express();
+
+app.use(express.json())
+
+app.post("/hdfcWebhook", async (req, res) => {
+    //TODO: Add zod validation here?
+    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
+    const paymentInformation: {
+        token: string;
+        userId: string;
+        amount: number
+    } = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    };
+
+    try {
+        await db.$transaction([
+            db.balance.update({
+                where: {
+                    userId: paymentInformation.userId
+                },
+                data: {
+                    amount: {
+                        // You can also get this from your DB
+                        increment: paymentInformation.amount
+                    }
+                }
+            }),
+            db.onRampTransaction.update({
+                where: {
+                    token: paymentInformation.token
+                }, 
+                data: {
+                    status: "Success",
+                }
+            })
+        ]);
+
+        res.json({
+            message: "Captured"
+        })
+    } catch(e) {
+        console.error(e);
+        await db.onRampTransaction.update({
+            where: {
+                token: paymentInformation.token
+            }, 
+            data: {
+                status: "Failure",
+            }
+        })
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
+
+})
+
+app.listen(3003, () => {
+    console.log(`Bank WebHook server is listening on Port ${3003}`);
+}); 
